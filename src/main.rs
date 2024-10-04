7d81b83d6533826d0ab2128a0c4aea76d3b50e5b
rafacostavilla <rafaelcostavilla@gmail.com>
2024-10-04 18:35:58 +1300

    Show the Rc counter for some cases

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use rust_smartpointers_rc_cons_sample::List::{Cons, Nil};
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("a: {:?}", a);
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
-
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{:?}", c);
+    println!("b: {:?}", b);
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("c: {:?}", c);
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
